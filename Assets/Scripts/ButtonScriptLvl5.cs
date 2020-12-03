97c5688d36b1cb1c6ed91227c5a094720d0aed43
Joan <joan.bosch.pons@estudiantat.upc.edu>
2020-12-03 20:29:54 +0100

    Camera changes!

---
+++ Assets/Scripts/ButtonScriptLvl5.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScriptLvl5 : MonoBehaviour
+{
+    public GameObject spike, placeholder;
+    List<GameObject> spikes;
+    List<GameObject> placeholders;
+
+    List<Vector3> positions;
+    List<bool> skipeEnabled;
+
+    //Button animations
+    int press = Animator.StringToHash("press");
+    int release = Animator.StringToHash("unpress");
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        initPositions();
+        initScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i<spikes.Count; ++i)
+        {
+            if (skipeEnabled[i]) spikes[i].active = true;
+            else spikes[i].active = false;
+        }
+        for (int i = 0; i < placeholders.Count; ++i)
+        {
+            if (!skipeEnabled[i]) placeholders[i].active = true;
+            else placeholders[i].active = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            switchSpikes();
+        }
+    }
+
+    void initPositions()
+    {
+        positions = new List<Vector3>();
+        positions.Add(new Vector3(-3.6f, 1.8f, 0.0f));
+        positions.Add(new Vector3(-3.6f, 0.608f, 0.0f));
+        positions.Add(new Vector3(-3.6f, 1.212f, 0.0f));
+
+        positions.Add(new Vector3(2.67f, 4.128f, 0.0f));
+        positions.Add(new Vector3(3.129f, 4.128f, 0.0f));
+
+        positions.Add(new Vector3(2.2922f, 0.091f, 0.0f));
+        positions.Add(new Vector3(2.2922f, 0.616f, 0.0f));
+        positions.Add(new Vector3(2.2922f, -0.432f, 0.0f));
+
+        positions.Add(new Vector3(9.39f, 0.536f, 0.0f));
+        positions.Add(new Vector3(9.36f, 0.03f, 0.0f));
+
+        positions.Add(new Vector3(3.406f, -0.96f, 0.0f));
+        positions.Add(new Vector3(3.905f, -0.96f, 0.0f));
+        positions.Add(new Vector3(4.486f, -0.96f, 0.0f));
+
+        positions.Add(new Vector3(3.763f, 1.209f, 0.0f));
+        positions.Add(new Vector3(4.375f, 1.209f, 0.0f));
+
+
+        skipeEnabled = new List<bool>();
+
+        skipeEnabled.Add(true);
+        skipeEnabled.Add(true);
+        skipeEnabled.Add(true);
+
+        skipeEnabled.Add(true);
+        skipeEnabled.Add(true);
+
+        skipeEnabled.Add(false);
+        skipeEnabled.Add(false);
+        skipeEnabled.Add(false);
+
+        skipeEnabled.Add(true);
+        skipeEnabled.Add(true);
+
+        skipeEnabled.Add(false);
+        skipeEnabled.Add(false);
+        skipeEnabled.Add(false);
+
+        skipeEnabled.Add(true);
+        skipeEnabled.Add(true);
+
+    }
+
+    void initScene()
+    {
+        spikes = new List<GameObject>();
+        placeholders = new List<GameObject>();
+
+        for (int i = 0; i < positions.Count; i++)
+        {
+            //Adding spikes
+            GameObject go = Instantiate(spike, positions[i], transform.rotation);
+            if (skipeEnabled[i]) go.active = false;
+            spikes.Add(go);
+
+            //Adding Meteors
+            go = Instantiate(placeholder, positions[i], transform.rotation);
+            if (!skipeEnabled[1]) go.active = false;
+            placeholders.Add(go);
+        }
+    }
+
+    public void switchSpikes()
+    {
+        anim.SetTrigger(press);
+        for (int i = 0; i < skipeEnabled.Count; ++i) skipeEnabled[i] = !skipeEnabled[i];
+    }
+}
---
+++ Assets/Scripts/Level5Cameras.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level5Cameras : MonoBehaviour
+{
+    public GameObject InitialCamera;
+    public GameObject Camera2;
+    public GameObject Camera3;
+    public GameObject Camera4;
+    public GameObject Camera5;
+    public GameObject Camera6;
+    public GameObject Camera7;
+    public GameObject Camera8;
+    public GameObject Camera9;
+    public GameObject Camera10;
+
+    private GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        InitialCamera.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.transform.position.x > 0)
+        {
+            InitialCamera.SetActive(false);
+            Camera2.SetActive(true);
+        }
+    }
+}
