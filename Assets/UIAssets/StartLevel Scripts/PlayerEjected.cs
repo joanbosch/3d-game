5c9311cfa03b688e21be0b78d75d4b4dde443100
Daniela Zhao <daniela.zhao@estudiantat.upc.edu>
2020-12-05 18:31:48 +0100

    Level1 transition scene

---
+++ Assets/UIAssets/StartLevel Scripts/PlayerEjected.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerEjected : MonoBehaviour
+{
+    public float movSpeed = 250.0f;
+
+    private Rigidbody rb;
+    private Vector3 eulerAnglVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = new Vector3(movSpeed, 0.0f, 0.0f);
+        eulerAnglVel = new Vector3(0, 0, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(eulerAnglVel * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
+        if (transform.position.x > 1400)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/UIAssets/StartLevel Scripts/TextWriter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    // to track position of player
+    public GameObject player;
+    public Text UImessage;
+
+    private bool writing;
+    private int charInd;
+    private float timePerChar = 0.1f;
+    private string textToWrite;
+    private float timer;
+
+    //sound
+    private AudioSource audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        writing = false;
+        charInd = 0;
+        audioManager = GetComponent<AudioSource>();
+        textToWrite = "pink was ejected.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("player pos " + player.transform.position);
+        if (player.transform.position.x > 470 && charInd == 0) writing = true;
+
+        if (writing)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0.0f)
+            {
+                //display next character
+                audioManager.Play();
+                timer += timePerChar;
+                charInd++;
+                string text = textToWrite.Substring(0, charInd);
+                text += "<color=#00000000>" + textToWrite.Substring(charInd) + "</color>";  // to keep all letters on its final pos
+                UImessage.text = text;
+
+                if (charInd >= textToWrite.Length) writing = false;
+            }
+        }
+    }
+}
