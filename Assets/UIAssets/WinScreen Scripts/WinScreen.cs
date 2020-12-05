b213af526e82aa896e8465a94ad329454ca99355
Daniela Zhao <daniela.zhao@estudiantat.upc.edu>
2020-12-05 20:04:32 +0100

    win screen

---
+++ Assets/UIAssets/WinScreen Scripts/WinScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game!");
+        Application.Quit();
+    }
+}
