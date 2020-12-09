abac5d76bed70c66063a3f0a15afc8a1b64f5af4
Joan <joan.bosch.pons@estudiantat.upc.edu>
2020-12-09 20:46:28 +0100

    Added Sounds in Main Menu

--- Assets/UIAssets/MainMenu Scripts/MainMenu.cs
+++ Assets/UIAssets/MainMenu Scripts/MainMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private AudioManager AudioManager;
+
+    void Start()
+    {
+        AudioManager = (AudioManager)FindObjectOfType(typeof(AudioManager));
+        AudioManager.Play("Pods");
+    }
     public void StartGame()
     {
+        
         // load next scene on Queue
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -20,5 +28,10 @@ public class MainMenu : MonoBehaviour
     public void LoadLevel(int i)
     {
         SceneManager.LoadScene(i);
+    }
+
+    public void playSoundSelect()
+    {
+        AudioManager.Play("Select");
     }
 }
